772a581c1169cd33faddae0267e6f14e94ffb10f
letamhot <letamhot@gmail.com>
2020-04-26 19:00:02 +0700

    ud

--- app/Http/Controllers/ShoesController.php
+++ app/Http/Controllers/ShoesController.php
@@ -26,12 +26,7 @@ use Illuminate\Http\Request;
 
 class ShoesController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('role:ROLE_ADMIN');
-        $this->middleware('role:ROLE_SUPERADMIN');
-    }
+
     public function home(Request $request)
     {
         // Cart::destroy();

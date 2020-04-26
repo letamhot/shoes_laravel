91c3480344361d3be089c53a07fd25a68707f72d
letamhot <letamhot@gmail.com>
2020-04-26 16:05:39 +0700

    ud

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // $this->call(ProductTableSeeder::class);
-        $this->call(SizeTableSeeder::class);
+        // $this->call(SizeTableSeeder::class);
         $this->call(GenderTableSeeder::class);
     }
 }
